894b22a0f898052a01ba630d27ae3fc411910de8
anakdrch <ana.kudrevich@gmail.com>
2024-12-28 21:04:12 +0100

    feat: implement getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -138,8 +138,15 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length > 0) {
+    const sumOfElements = arr.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    return Number((sumOfElements / arr.length).toFixed(2));
+  }
+  return 0;
 }
 
 /**
