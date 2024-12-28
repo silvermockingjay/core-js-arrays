6483a33f8f87c4b3641d623d40683cfa2033f572
anakdrch <ana.kudrevich@gmail.com>
2024-12-28 18:37:50 +0100

    feat: implement sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,24 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let sumArray = [];
+  if (arr1.length >= arr2.length) {
+    sumArray = arr1.map((item, index) => {
+      if (index < arr2.length) {
+        return item + arr2[index];
+      }
+      return item;
+    });
+    return sumArray;
+  }
+  sumArray = arr2.map((item, index) => {
+    if (index < arr1.length) {
+      return item + arr1[index];
+    }
+    return item;
+  });
+  return sumArray;
 }
 
 /**
