e12f8bd4875fd4cd3553ae1a9532c599cbc3f15d
anakdrch <ana.kudrevich@gmail.com>
2024-12-28 21:41:53 +0100

    feat: implement distinct

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -271,8 +271,9 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const mySet = new Set(arr);
+  return Array.from(mySet);
 }
 
 /**
