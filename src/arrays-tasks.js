b06939d6a9351bceb71dcb7dfb05855e027aa498
anakdrch <ana.kudrevich@gmail.com>
2024-12-28 21:12:20 +0100

    feat: implement isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -159,8 +159,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const lengthOfElement = arr[0].length;
+  return arr.every((item) => item.length === lengthOfElement);
 }
 
 /**
