349ae80fd9f9f4fb5c4b8f0351c7f0760816db25
anakdrch <ana.kudrevich@gmail.com>
2024-12-31 00:36:01 +0100

    feat: implement getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -469,8 +469,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const indexes = numbers.map((item, index) => index);
+  const answer = indexes.filter((index) => numbers[index] % 2 !== 0);
+  return answer;
 }
 
 /**
