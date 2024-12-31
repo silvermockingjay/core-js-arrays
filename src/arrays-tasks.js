e33e01cec20aafcd22c9dceaa440d3f4ffa35bd3
anakdrch <ana.kudrevich@gmail.com>
2024-12-31 02:19:33 +0100

    feat: implement shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -573,8 +573,16 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  let index = 0;
+  if (n > 0) {
+    index = arr.length - n;
+  } else {
+    index = Math.abs(n);
+  }
+  const head = arr.slice(0, index);
+  const tail = arr.slice(index);
+  return tail.concat(head);
 }
 
 /**
