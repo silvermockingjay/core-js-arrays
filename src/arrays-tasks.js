4ce5edf715e1e6cc8cc5b34e9078c4b215bdc763
anakdrch <ana.kudrevich@gmail.com>
2024-12-31 01:43:22 +0100

    feat: implement sortedDigits

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -588,8 +588,20 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const sortedDigits = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.sort((a, b) => sortedDigits.indexOf(a) - sortedDigits.indexOf(b));
 }
 
 /**
